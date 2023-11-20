72d6f122cd3c519f6148ac46a9290a3944128a59
Alek Miko <alekmikopro@gmail.com>
2023-11-20 11:18:48 +0100

    adding the modication from the 1st quest

--- public/index.php
+++ public/index.php
@@ -5,8 +5,12 @@
 
 require __DIR__ . '/../src/Animal.php';
 
-// $animal1 = new Animal();
-// $animal2 = new Animal();
+$animal1 = new Animal();
+$animal2 = new Animal();
+$animal3 = new Animal();
+var_dump($animal1);
+var_dump($animal2);
+var_dump($animal3);
 
 // $animals = [$animal1, $animal2];
 
